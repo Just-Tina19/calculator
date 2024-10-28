48f21e840a3a3c9fbdd14de24e1278de3228bc89
My-website <140087285+Just-Tina19@users.noreply.github.com>
2024-10-28 15:54:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const display = document.getElementById("dizplay");
+
+function appendToDisplay(input){
+     display.value += input;
+}
+
+function clearDisplay(){
+     display.value = '';
+}
+
+function calculate(){
+     try{
+          display.value = eval(display.value);
+     }
+
+     
+     catch(error){
+          display.value = "Error";
+      }
+}
